f8240132970f1e89a8c71fac86ddec0204f8feff
daveadi <adityadave004@gmail.com>
2026-08-13 18:18:11 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import Perceptron
 from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
-
+# Generate a synthetic dataset for binary classification with 10 features, 1000 samples
 x, y = make_classification(
     n_samples=1000,
     n_features=10,
