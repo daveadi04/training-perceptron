75d878f8c275d166e1c3720c7b0bf136f0c64052
daveadi <adityadave004@gmail.com>
2026-08-13 18:17:15 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import Perceptron
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+x, y = make_classification(
+    n_samples=1000,
+    n_features=10,
+    n_classes=2,
+    random_state=42
+)
+
+
